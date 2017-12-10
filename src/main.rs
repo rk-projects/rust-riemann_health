e5d0cd667d5f588052dbe0ce3b3c4e0435b76ebe
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-12-10 10:42:34 +0530

    attributes added

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use libc::timeval;
 use sys_info::*;
 use riemann_client::Client;
 use riemann_client::proto::Event;
-//use riemann_client::proto::Attribute;
+use riemann_client::proto::Attribute;
 //use protobuf::repeated::RepeatedField;
 
 
@@ -26,10 +26,13 @@ fn main() {
  };
 println!("{}", boottime.tv_sec);
 
-// let mut att = RepeatedField::<Attribute>::new();
+let mut att = Attribute::new();
+let mut att_vec :Vec<Attribute> = Vec::new();
 
-// att.key = "boottime";
-// att.value = boottime.tv_sec;
+att.set_key("boottime".to_string());
+att.set_value(boottime.tv_sec.to_string());
+att_vec.push(att);
+
 
 let mut client = Client::connect(&("localhost", 5555)).unwrap();
 
@@ -40,15 +43,6 @@ let mut client = Client::connect(&("localhost", 5555)).unwrap();
 //     event.set_metric_d(boottime.tv_sec as f64);
 //     event
 // }).unwrap();
-
-client.event({
-    let mut event = Event::new();
-    event.set_service("boottime".to_string());
-    event.set_state("OK".to_string());
-    //event.set_attributes(att);
-    event.set_metric_d(boottime.tv_sec as f64);
-    event
-}).unwrap();
 
 
 
@@ -64,6 +58,23 @@ let disk_info = match disk_info{
  };
 println!("{}", disk_info.total);
 println!("{}", disk_info.free);
+
+let mut disk_vec :Vec<Attribute> = Vec::new();
+let mut disk_att = Attribute::new();
+disk_att.set_key("total disk".to_string());
+disk_att.set_value(disk_info.total.to_string());
+att_vec.push(disk_att);
+
+
+client.event({
+    let mut event = Event::new();
+    event.set_service("boottime".to_string());
+    event.set_state("OK".to_string());
+    event.set_attributes(protobuf::RepeatedField::from_vec(att_vec));
+    event.set_metric_d(boottime.tv_sec as f64);
+    event
+}).unwrap();
+
 
 
 }
